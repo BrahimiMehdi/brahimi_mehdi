c1b5b20f5694412a8f7033da0b056da0b70f6ced
BrahimiMehdi <brahimi.mehdi37@gmail.com>
2023-01-01 23:29:55 +0100

    added hover animations

--- components/Header.jsx
+++ components/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
         <div data-line="2" className="w-[2px] sideLine bg-yellow h-0 absolute right-0"></div>
         <div data-info="1" className="w-full innerInfo opacity-0 h-full py-4 px-12 flex items-center justify-between">
             <Image width={400} height={400} className="w-20 object-contain" src={"/pic.png"} />
-            <p className="text-xl font-bold uppercase">brahimi mehdi</p>
+            <p className="text-xl font-bold uppercase tracking-widest">brahimi mehdi</p>
             <Link href={"/"}>
                 <AiFillHome className="text-3xl text-white" />
             </Link>
@@ -29,19 +29,19 @@ const Header = () => {
       </div>
       <div className="w-full h-full relative ">
         <div data-line="3" className="w-[2px] bg-yellow sideLine h-0 absolute right-0"></div>
-        <LinkS data-info="2" to="/" className="h-full tracking-widest opacity-0 w-full  font-semibold innerInfo cursor-pointer uppercase grid place-items-center">home</LinkS>
+        <LinkS data-info="2" to="/" className="h-full transition-transform duration-300 ease-out hover:scale-90 text-sm tracking-widest opacity-0 w-full  font-semibold innerInfo cursor-pointer uppercase grid place-items-center">home</LinkS>
       </div>
       <div className="w-full h-full relative ">
         <div data-line="4" className="w-[2px] bg-yellow sideLine h-0 absolute right-0"></div>
-        <LinkS data-info="3" to="/" className="h-full tracking-widest opacity-0 w-full  font-semibold cursor-pointer innerInfo uppercase grid place-items-center">work</LinkS>
+        <LinkS data-info="3" to="/" className="h-full transition-transform duration-300 ease-out hover:scale-90 text-sm tracking-widest opacity-0 w-full  font-semibold cursor-pointer innerInfo uppercase grid place-items-center">work</LinkS>
       </div>
       <div className="w-full h-full relative ">
         <div data-line="5" className="w-[2px] bg-yellow sideLine h-0 absolute right-0"></div>
-        <LinkS data-info="5" to="/" className="h-full tracking-widest opacity-0 w-full  font-semibold cursor-pointer innerInfo uppercase grid place-items-center">about</LinkS>
+        <LinkS data-info="5" to="/" className="h-full transition-transform duration-300 ease-out hover:scale-90 text-sm tracking-widest opacity-0 w-full  font-semibold cursor-pointer innerInfo uppercase grid place-items-center">about</LinkS>
       </div>
       <div className="w-full h-full relative ">
         <div data-line="6" className="w-[2px] bg-yellow sideLine h-0 absolute right-0"></div>
-        <LinkS data-info="6" to="/" className="h-full tracking-widest opacity-0 w-full  font-semibold cursor-pointer innerInfo uppercase grid place-items-center">
+        <LinkS data-info="6" to="/" className="h-full transition-transform duration-300 ease-out hover:scale-90 text-sm tracking-widest opacity-0 w-full  font-semibold cursor-pointer innerInfo uppercase grid place-items-center">
             contact
         </LinkS>
       </div>
