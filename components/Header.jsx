b5be966540bbb11227b241cc6ee2c81477a1b41f
BrahimiMehdi <brahimi.mehdi37@gmail.com>
2023-02-12 18:10:26 +0100

    updated pfp

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
         <div data-line="1" className="w-[1px] sideLine bg-yellow h-0 absolute left-0"></div>
         <div data-line="2" className="w-[1px] sideLine bg-yellow h-0 absolute left-0"></div>
         <div data-info="1" className="w-full innerInfo opacity-0 h-full py-4 md:px-12 flex items-center justify-center md:justify-between">
-            <Image alt="picture contains my face" priority width={400} height={400} className="w-20 md:block hidden object-contain" src={"/pic.png"} />
+            <Image alt="picture contains my face" priority width={400} height={400} className="w-20 h-20 rounded-full object-center object-cover md:block hidden" src={"/pfp.jpeg"} />
             <p className="text-xl md:block hidden font-bold uppercase tracking-widest">brahimi mehdi</p>
             <Link href={"/"}>
                 <AiFillHome className="text-3xl text-white" />
